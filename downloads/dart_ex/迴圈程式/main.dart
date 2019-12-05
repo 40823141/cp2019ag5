aaab0b99d2989b1b119efd58172bef7af6890c7c
cp2019ag5 <40823102@gm.nfu.edu.tw>
2019-12-05 09:30:41 +0800

    initial add

--- downloads/dart_ex/迴圈程式/main.dart
+++ downloads/dart_ex/迴圈程式/main.dart
@@ -1,8 +1,18 @@
-main(){
+main() {
   List temp = ["20C", "30C", "50F", "40C", "23F"];
   for (var i in temp) {
-    //print(i);
-    //print(i[2]);
-    if (i[2] == "C") print(i[0] + i[1]);
-  } // for
+  
+    if (i[2] == "C") {
+    var a = (i[0]+i[1]);
+     var c = int.parse (a);
+    double f =c*9/5+32;
+    print ('F=$f');
+    }
+    else{
+      var a = (i[0]+i[1]);
+     var f = int.parse (a);
+    double c =(f-32)*5/9;
+    print ('C=$c');
+    }
+  } 
 }
